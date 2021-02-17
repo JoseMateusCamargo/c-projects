e5058ab3ebc157fd9a5bde45821c8e809c237903
De Leva <Jose.Leva@cevalogistics.com>
2021-02-17 15:06:48 -0300

    first commit

---
+++ factorial_simple.cpp
@@ -0,0 +1,23 @@
+//
+// Created by JMC on 15/02/2021.
+// Simple factorial calculator
+//
+
+#include <stdio.h>
+#include <math.h> // to use abs()
+
+int main() {
+    int n, i = 2, fat = 1;
+    printf("\nFactorial of: ");
+    scanf("%d", &n);
+    n = abs(n);
+
+    while (i <= n) {
+        fat = fat * i;
+        i = i + 1;
+    }
+
+    printf("Result = %d", fat);
+    getchar();
+    return 0;
+}
---
+++ media_simples.cpp
@@ -0,0 +1,26 @@
+//
+// Created by JMC on 15/02/2021.
+// simples calculo de media de 4 objeto, exibe a média do aluno, e a média geral.
+//
+
+#include <stdio.h>
+
+int main() {
+    float n1, n2, n3, n4;
+    double ma, mg;
+    int i;
+    for (i = 1; i <= 3; i++) {
+        ma = 0;
+        printf("\nDigite as 04 notas: %d\n", i);
+        scanf("%f%f%f%f", &n1, &n2, &n3, &n4);
+        ma = (n1 + n2 + n3 + n4) / 4;
+        printf("\nMedia = %f", ma);
+    }
+
+    mg = ma;
+    mg /= 3;
+    printf("\nMedia geral= %1.4f", mg);
+    fflush(stdin);
+    getchar();
+    return 0;
+}
